17251843952752f313c5d120d5466970354fda79
Leonardho R Sitanggang <79736302+FlazeFy@users.noreply.github.com>
2024-06-02 01:28:18 +0700

    [Telegram-BOT] See stats inventoru price by context

--- services/module/stats/stats_queries.py
+++ services/module/stats/stats_queries.py
@@ -1,9 +1,13 @@
-from services.module.stats.template import get_total_item_by_context
+from services.module.stats.template import get_total_item_by_context, get_custom_aggregate
+from helpers.converter import convert_price_number
 
 async def get_stats():
     dt_total_inventory_by_category = await get_total_item_by_context(tableName="inventory", targetColumn="inventory_category")
     dt_total_inventory_by_room = await get_total_item_by_context(tableName="inventory", targetColumn="inventory_room")
     dt_total_inventory_by_favorite = await get_total_item_by_context(tableName="inventory", targetColumn="is_favorite")
+    dt_price_inventory_by_category = await get_custom_aggregate(tableName="inventory", targetColumn="inventory_category", extra="SUM(inventory_price) as total")
+    dt_price_inventory_by_room = await get_custom_aggregate(tableName="inventory", targetColumn="inventory_room", extra="SUM(inventory_price) as total")
+    dt_price_inventory_by_favorite = await get_custom_aggregate(tableName="inventory", targetColumn="is_favorite", extra="SUM(inventory_price) as total")
 
     res = f"<b>Total Inventory By Category:</b>\n"
     for dt in dt_total_inventory_by_category:
@@ -23,5 +27,24 @@ async def get_stats():
             res += f"- Normal Item : {dt.total}\n"
         else:
             res += f"- Favorite : {dt.total}\n"
+
+    res += f"\n<b>Total Price Inventory By Category:</b>\n"
+    for dt in dt_price_inventory_by_category:
+        res += (
+            f"- {dt.context} : Rp. {convert_price_number(dt.total)},00\n"
+        )
+
+    res += f"\n<b>Total Price Inventory By Room:</b>\n"
+    for dt in dt_price_inventory_by_room:
+        res += (
+            f"- {dt.context} : Rp. {convert_price_number(dt.total)},00\n"
+        )
+
+    res += f"\n<b>Total Price Inventory By Is Favorite:</b>\n"
+    for dt in dt_price_inventory_by_favorite:
+        if dt.context == 0:
+            res += f"- Normal Item : Rp. {convert_price_number(dt.total)},00\n"
+        else:
+            res += f"- Favorite : Rp. {convert_price_number(dt.total)},00\n"
     return res
 
--- services/module/stats/template.py
+++ services/module/stats/template.py
@@ -16,3 +16,19 @@ async def get_total_item_by_context(tableName, targetColumn):
     data = result.fetchall()
 
     return data
+
+async def get_custom_aggregate(tableName, targetColumn, extra):
+    # Query builder
+    sql_query = f"""
+        SELECT {targetColumn} AS context, {extra}
+        FROM {tableName}
+        GROUP BY 1
+        ORDER BY 2
+    """
+    compiled_sql = text(sql_query)
+
+    # Exec
+    result = con.execute(compiled_sql)
+    data = result.fetchall()
+
+    return data
